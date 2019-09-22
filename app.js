9dea57ba4e26bf8906e9885e780e677e45d7d2e0
SeaFalcon <seafalcon323@gmail.com>
2019-09-23 06:45:44 +0900

    20190923

--- app.js
+++ app.js
@@ -3,22 +3,29 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 // 연결을 끊을 수 있음 send쓰면..
 const middleware = (req, res, next) => {
   res.send("not happening");
 };
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.listen(4000, () => console.log("Listening on http://localhost:4000..."));
+export default app;
